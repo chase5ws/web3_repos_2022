cb9b556961b3396439486b7f5ccca88f84845166
Chase5ws <55267518+chase5ws@users.noreply.github.com>
2022-10-10 02:53:21 +0800

    Update ASC_ver1.sol

--- ASC_ver1.sol
+++ ASC_ver1.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ASC_ver1.0 is ERC721Enumerable, Ownable {
+contract ASC_ver1 is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     bool public _isSaleActive = false;
